54da097032f8afb787440b633541b7f4ce836e7f
Thomas Minot <tms.minot@gmail.com>
2017-09-10 23:09:30 +0000

    struggling with MXnet

--- pipeline.py
+++ pipeline.py
@@ -7,16 +7,18 @@ import numpy as np
 
 sym_path = 'model/Inception-7-symbol.json'
 param_path = 'model/Inception-7-0001.params'
-batch_size = 40
-thr = 0.95
+batch_size = 10
+thr = 0.5
 
 
 def build_module():
     ### This function builds the MXnet model of Inception V3 ###
     
     sym = mx.sym.load(sym_path)                                             # load symbol    
-    mod = mx.mod.Module(sym, context=mx.cpu())                     # instantiate MXNet module
-    mod.bind([("data",(batch_size,3,299,299))], for_training=False)   # bind to memory, only for inference
+    mod = mx.mod.Module(sym, context=mx.cpu())                              # instantiate MXNet module
+    mod.bind([("data",(batch_size,3,299,299))],
+             [("softmax_label",(batch_size,))], 
+             for_training=False)                                            # bind to memory, only for inference
     mod.load_params(param_path)                                             # load pretrained weights    
     return mod
     
@@ -24,35 +26,36 @@ def build_module():
 def process_images(img_list, module):
     ### This function infers batches of data ###
     
-    data = np.stack([im['data'] for im in img_list])                        
-    eval_data = mx.io.NDArrayIter(data, batch_size=batch_size)              # data iterator
+    data = np.concatenate([im['data'] for im in img_list], axis=0)  
+    labels = np.ones(data.shape[0])                                         # so that the engine doesn't complain
+    eval_data = mx.io.NDArrayIter(data, labels, batch_size=batch_size)      # data iterator
     pred = module.predict(eval_data)                                        # predictions from softmax layer
-    return pred
+    return pred                                                             # contains 8 junk classes, appended at the end
 
     
 def preprocess_img(img):
     ### Preprocessing for Inception V3 ###
     
     h,w = img.shape[:2]
-    
     crop = np.min([h,w])
-    img = img.transpose((2, 0, 1))                                          # channels first
-    img = img[:, (h-crop)//2:(h+crop)//2, (w-crop)//2:(w+crop)//2]          # crop
-    img = imresize(img, (299,299))                                    # resize for first layer
+    img = img[(h-crop)//2:(h+crop)//2, (w-crop)//2:(w+crop)//2, :]          # crop
+    img = imresize(img, (299,299))                                          # resize for first layer
     img = img.astype('float32')
     img /= 255                                                      
     img -= 0.5
     img *= 2.
+    img = img.transpose((2, 0, 1))                                          # channels first    
     img = np.expand_dims(img, axis=0)                                       # add dimension for batch
     return img
     
-def make_results(img_list, pred, thr):
+def make_results(img_list, pred):
     ### This function assembles the results from the predictions ###
     
     syn = []
     with open('model/synset.txt') as f:                                     # ImageNet synset names
-        for line in f[1:]:
-            syn.append(line[10:].split(' ', 1)[0])                          # first word in description 
+        f.next()
+        for line in f:
+            syn.append(line[10:].split(',', 1)[0].split('\n',1)[0])                          # first word in description 
             
     j = 0
     for im in img_list:
@@ -61,7 +64,7 @@ def make_results(img_list, pred, thr):
             j +=1
         else:
             im['Error'] = 'Invalid URL'
-        im.pop('data')
+        im.pop('data', None)
     
     return img_list
     
@@ -69,9 +72,11 @@ def load_img(url):
     ### This function downloads an numpy image ###
     try:
         img = io.imread(url)
+        assert len(img.shape) is 3
         assert img.shape[2] is 3
     except:
         print "Invalid data or url"
+        img = None
     
     return img
         
@@ -82,7 +87,8 @@ def load_imgs(url_list):
     for u in url_list:
         img_list.append({})
         img_list[-1]['url'] = u
-        img_list[-1]['data'] = load_img(u)
+        img = load_img(u)
+        img_list[-1]['data'] = preprocess_img(img) if img is not None else None
     
     return img_list
 
