77ed9e1b8c24d9ca4a0578fb6b0b76cdb27fced1
Thomas Minot <tms.minot@gmail.com>
2017-09-10 10:00:50 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+import pipeline
+from celery import Celery
+
+
+app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379')
+celery = make_celery(app)
+
+
+model = pipeline.build_model()
+
+
+@app.route('/api/infer', methods=['POST'])
+def post_data(payload):
+    results = worker_process(payload)
+    return jsonify(payload)
+
+
+@celery.task()
+def worker_process(payload):
+    return a + b
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
---
+++ pipeline.py
@@ -0,0 +1,18 @@
+import mxnet as mx
+import json
+import csv
+import rabbitmq as mq
+
+def build_model():
+    mod = mx.Symbol.load()
+
+def infer():
+    
+    
+def preprocess_img():
+    
+def cache_imgs():
+    
+def cache_img():
+
+def 
